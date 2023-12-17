e4df7f99a5f4ec5a616b8d1852669f91aa582452
Božo Stojković <bozo.stojkovic@gmail.com>
2023-12-17 21:54:08 +0100

    first commit

---
+++ src/main/java/com/mementis/mementis/MementisApplication.java
@@ -0,0 +1,13 @@
+package com.mementis.mementis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MementisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MementisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mementis/mementis/MementisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mementis.mementis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MementisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
